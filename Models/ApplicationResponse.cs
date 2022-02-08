c5d8464816aa8dcf5a5b377f99ec4814f42c3e63
Kevin <ktroll@byu.edu>
2022-02-07 19:10:39 -0700

    update

--- Models/ApplicationResponse.cs
+++ Models/ApplicationResponse.cs
@@ -6,6 +6,9 @@ namespace QuadrantsProject.Models
 {
     public class ApplicationResponse
     {
+        [Required]
+        [Key]
+        private int TaskID { get; set; }
 
         [Required]
         public string Task { get; set; }
